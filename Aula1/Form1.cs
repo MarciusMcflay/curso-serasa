c929d73fb72f6e9289f834e586379e8de733d139
LUIZA MARIA DA SILVA <luizamaria.silva113@gmail.com>
2023-07-20 20:12:28 -0300

    coloquei um botão com a cor MistyRose que ao clica-lo muda o texto da label para luma.

--- Aula1/Form1.cs
+++ Aula1/Form1.cs
@@ -26,5 +26,10 @@ namespace Aula1
         {
             lblName.Text = "Marcius Leandro";
         }
+
+        private void btnLuiza_Click(object sender, EventArgs e)
+        {
+            lblName.Text = "Luma";
+        }
     }
 }
