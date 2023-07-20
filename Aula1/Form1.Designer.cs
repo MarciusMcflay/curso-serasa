90733496533b197d6d1b572af534c5e7dea646b9
MatheusCamarg123 <camargomatheus543@gmail.com>
2023-07-20 20:54:28 -0300

    Alterei o nome do botão

--- Aula1/Form1.Designer.cs
+++ Aula1/Form1.Designer.cs
@@ -31,7 +31,7 @@ namespace Aula1
         {
             this.lblName = new System.Windows.Forms.Label();
             this.btnProfessor = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnMatheusC = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblName
@@ -55,22 +55,22 @@ namespace Aula1
             this.btnProfessor.UseVisualStyleBackColor = true;
             this.btnProfessor.Click += new System.EventHandler(this.btnProfessor_Click);
             // 
-            // button1
+            // btnMatheusC
             // 
-            this.button1.Location = new System.Drawing.Point(535, 289);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(85, 30);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Matheus C.";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnMatheusC.Location = new System.Drawing.Point(535, 289);
+            this.btnMatheusC.Name = "btnMatheusC";
+            this.btnMatheusC.Size = new System.Drawing.Size(85, 30);
+            this.btnMatheusC.TabIndex = 2;
+            this.btnMatheusC.Text = "Matheus C.";
+            this.btnMatheusC.UseVisualStyleBackColor = true;
+            this.btnMatheusC.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1080, 554);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnMatheusC);
             this.Controls.Add(this.btnProfessor);
             this.Controls.Add(this.lblName);
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
@@ -86,7 +86,7 @@ namespace Aula1
 
         private System.Windows.Forms.Label lblName;
         private System.Windows.Forms.Button btnProfessor;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnMatheusC;
     }
 }
 
