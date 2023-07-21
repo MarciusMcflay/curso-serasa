974095b9cae96dc936eca19ed1a20b218d7d98ac
ISABELE DOS SANTOS RIBEIRO <isabele.sribeiro@senacsp.edu.br>
2023-07-20 21:06:53 -0300

    Alterei o nome do botão dessa vez está certo

--- Aula1/Form1.cs
+++ Aula1/Form1.cs
@@ -27,7 +27,7 @@ namespace Aula1
             lblName.Text = "Marcius Leandro";
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnIsabele_Click(object sender, EventArgs e)
         {
             lblName.Text = "Isabele Ribeiro";
         }
