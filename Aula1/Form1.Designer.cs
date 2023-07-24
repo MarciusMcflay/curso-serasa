313ccb0729fcce4623b70d8ebad95fe88d85100e
GABRIEL FRAZAO BRAZ <gabrielftot@gmail.com>
2023-07-24 20:11:25 -0300

    Botão abre um novo formulário

--- Aula1/Form1.Designer.cs
+++ Aula1/Form1.Designer.cs
@@ -36,7 +36,7 @@ namespace Aula1
             // lblName
             // 
             this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(260, 483);
+            this.lblName.Location = new System.Drawing.Point(876, 433);
             this.lblName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblName.Name = "lblName";
             this.lblName.Size = new System.Drawing.Size(35, 13);
@@ -45,7 +45,7 @@ namespace Aula1
             // 
             // btnGabriel
             // 
-            this.btnGabriel.Location = new System.Drawing.Point(178, 82);
+            this.btnGabriel.Location = new System.Drawing.Point(140, 423);
             this.btnGabriel.Name = "btnGabriel";
             this.btnGabriel.Size = new System.Drawing.Size(75, 23);
             this.btnGabriel.TabIndex = 1;
@@ -60,7 +60,7 @@ namespace Aula1
             this.ClientSize = new System.Drawing.Size(1080, 554);
             this.Controls.Add(this.btnGabriel);
             this.Controls.Add(this.lblName);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
