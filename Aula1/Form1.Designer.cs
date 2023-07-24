32acb9b9e8a87aaaff9c46a82fe72c4ab8ab5fb1
bertoloteDev <bertolotematheus@gmail.com>
2023-07-24 20:11:21 -0300

    Botão abre um novo formulário

--- Aula1/Form1.Designer.cs
+++ Aula1/Form1.Designer.cs
@@ -47,7 +47,7 @@ namespace Aula1
             // btnProfessor
             // 
             this.btnProfessor.Location = new System.Drawing.Point(830, 478);
-            this.btnProfessor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnProfessor.Margin = new System.Windows.Forms.Padding(2);
             this.btnProfessor.Name = "btnProfessor";
             this.btnProfessor.Size = new System.Drawing.Size(56, 19);
             this.btnProfessor.TabIndex = 1;
@@ -57,7 +57,7 @@ namespace Aula1
             // 
             // Matheus
             // 
-            this.Matheus.Location = new System.Drawing.Point(411, 280);
+            this.Matheus.Location = new System.Drawing.Point(410, 280);
             this.Matheus.Name = "Matheus";
             this.Matheus.Size = new System.Drawing.Size(75, 21);
             this.Matheus.TabIndex = 2;
@@ -73,7 +73,7 @@ namespace Aula1
             this.Controls.Add(this.Matheus);
             this.Controls.Add(this.btnProfessor);
             this.Controls.Add(this.lblName);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
--- Aula1/Form1.cs
+++ Aula1/Form1.cs
@@ -30,6 +30,17 @@ namespace Aula1
         private void Matheus_Click(object sender, EventArgs e)
         {
             lblName.Text = "Matheus Bertolote";
+            Form frmMatheusBertolote = new frmMatheusBertolote();
+            // Coloquei o novo formulário no código. O "new" significa novo e coloquei através de um objeto que tem todas as ações, e o objeto foi colocado em uma variável. O frmMatheusBertolote é uma variável que precisa ser carregada por isso ela está declarada.
+            // Tela criada através do código
+
+            frmMatheusBertolote.WindowState = FormWindowState.Maximized;
+            //WindowState pega o status da minha janela, ela guarda o status que ela está. O maximized significa que a janela vai abrir no máximo sem mudar os botões e outras ferramentas
+
+            frmMatheusBertolote.Show();
+            // Mostrar o formulário na tela através do "show"
+            
+            
         }
     }
 }
