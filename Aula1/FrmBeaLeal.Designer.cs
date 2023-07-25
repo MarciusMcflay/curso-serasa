12158f32a30891f16ed154a43607a4e47af38823
BEATRIZ LEAL <beatriz.leal@senacsp.edu.br>
2023-07-24 21:27:20 -0300

    Adicionei componente de data data de nascimento

--- Aula1/FrmBeaLeal.Designer.cs
+++ Aula1/FrmBeaLeal.Designer.cs
@@ -28,19 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dTPBDataNascimento = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCadastrar = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // dTPBDataNascimento
+            // 
+            this.dTPBDataNascimento.Location = new System.Drawing.Point(189, 55);
+            this.dTPBDataNascimento.Name = "dTPBDataNascimento";
+            this.dTPBDataNascimento.Size = new System.Drawing.Size(213, 20);
+            this.dTPBDataNascimento.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Data de nascimento";
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadastrar.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.Location = new System.Drawing.Point(696, 405);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastrar.TabIndex = 2;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
             // 
             // FrmBeaLeal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dTPBDataNascimento);
             this.Name = "FrmBeaLeal";
             this.Text = "Cadastro do curriculo da Beatriz Leal";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DateTimePicker dTPBDataNascimento;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCadastrar;
     }
 }
