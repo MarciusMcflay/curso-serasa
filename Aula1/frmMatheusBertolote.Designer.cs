99213b88f25b84ca790a0531557aeb94eea7a4db
bertoloteDev <bertolotematheus@gmail.com>
2023-07-24 21:26:26 -0300

    Adicionei ferramentos ao novo formulário

--- Aula1/frmMatheusBertolote.Designer.cs
+++ Aula1/frmMatheusBertolote.Designer.cs
@@ -29,19 +29,69 @@ namespace Aula1
         /// </summary>
         private void InitializeComponent()
         {
+            this.Date = new System.Windows.Forms.DateTimePicker();
+            this.Birth = new System.Windows.Forms.Label();
+            this.Register = new System.Windows.Forms.Button();
+            this.DatadeNascimento = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // Date
+            // 
+            this.Date.Location = new System.Drawing.Point(214, 103);
+            this.Date.Name = "Date";
+            this.Date.Size = new System.Drawing.Size(226, 20);
+            this.Date.TabIndex = 0;
+            this.Date.Value = new System.DateTime(2023, 7, 24, 20, 56, 46, 0);
+            // 
+            // Birth
+            // 
+            this.Birth.AutoSize = true;
+            this.Birth.Location = new System.Drawing.Point(73, 109);
+            this.Birth.Name = "Birth";
+            this.Birth.Size = new System.Drawing.Size(107, 13);
+            this.Birth.TabIndex = 1;
+            this.Birth.Text = "Data de Nascimento:";
+            // 
+            // Register
+            // 
+            this.Register.Location = new System.Drawing.Point(229, 188);
+            this.Register.Name = "Register";
+            this.Register.Size = new System.Drawing.Size(75, 23);
+            this.Register.TabIndex = 2;
+            this.Register.Text = "Cadastrar";
+            this.Register.UseVisualStyleBackColor = true;
+            this.Register.Click += new System.EventHandler(this.Register_Click);
+            // 
+            // DatadeNascimento
+            // 
+            this.DatadeNascimento.AutoSize = true;
+            this.DatadeNascimento.Location = new System.Drawing.Point(482, 110);
+            this.DatadeNascimento.Name = "DatadeNascimento";
+            this.DatadeNascimento.Size = new System.Drawing.Size(35, 13);
+            this.DatadeNascimento.TabIndex = 3;
+            this.DatadeNascimento.Text = "label1";
             // 
             // frmMatheusBertolote
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DatadeNascimento);
+            this.Controls.Add(this.Register);
+            this.Controls.Add(this.Birth);
+            this.Controls.Add(this.Date);
             this.Name = "frmMatheusBertolote";
             this.Text = "Cadastro do Currículo Matheus Bertolote ";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DateTimePicker Date;
+        private System.Windows.Forms.Label Birth;
+        private System.Windows.Forms.Button Register;
+        private System.Windows.Forms.Label DatadeNascimento;
     }
 }
--- Aula1/frmMatheusBertolote.cs
+++ Aula1/frmMatheusBertolote.cs
@@ -16,5 +16,12 @@ namespace Aula1
         {
             InitializeComponent();
         }
+
+        private void Register_Click(object sender, EventArgs e)
+        {
+            string DatadeNascimento = Date.Value.ToString("yyyy-MM-dd");
+            // Criei uma string chamada data de nascimento, onde a data vai ser convertida em string. Como string é uma função é necessário(). "YYYY" significa 4 digitos em questão do ano. "MM" significa o mês em 2 digítos. "dd" significa dias em 2 digítos
+            this.DatadeNascimento.Text = DatadeNascimento;
+        }
     }
 }
