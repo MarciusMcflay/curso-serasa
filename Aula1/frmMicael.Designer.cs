33f66b7594c62680dd46c953177e07663964f5e1
MicaelNCosta <micael.nc01@gmail.com>
2023-07-24 21:26:42 -0300

    Adicionei componente de data, para salvar a data de nascimento

--- Aula1/frmMicael.Designer.cs
+++ Aula1/frmMicael.Designer.cs
@@ -29,19 +29,56 @@ namespace Aula1
         /// </summary>
         private void InitializeComponent()
         {
+            this.dTPBirthDate = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCadastrar = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // dTPBirthDate
+            // 
+            this.dTPBirthDate.Location = new System.Drawing.Point(140, 55);
+            this.dTPBirthDate.Name = "dTPBirthDate";
+            this.dTPBirthDate.Size = new System.Drawing.Size(200, 20);
+            this.dTPBirthDate.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Data de nascimento:";
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(678, 388);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastrar.TabIndex = 2;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
             // 
             // frmMicael
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dTPBirthDate);
             this.Name = "frmMicael";
             this.Text = "cadastro do curriculo Micael";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DateTimePicker dTPBirthDate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCadastrar;
     }
 }
